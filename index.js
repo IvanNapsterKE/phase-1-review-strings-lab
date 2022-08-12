0e75c2a68a22ce3f0c6da2071625c09475814da1
Ivan <nguyoivan@gmail.com>
2022-08-12 14:07:15 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser="Thunder";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const restofName=currentUser.slice(0,1);
+
+const shortGreeting= `Welcome, ${currentUser.slice(0,1)}!`;
